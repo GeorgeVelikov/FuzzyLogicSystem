522d0bb40cc78b44d05a210958800d75dbf91cb8
Georgi <45877509+GeorgeVelikov@users.noreply.github.com>
2020-12-07 13:44:41 +0000

    Revert "remove confused code"
    This reverts commit 44013ee70f0f0a76138b38c27aa92a7956c74453.

--- FuzzyLogicApp/fuzzy_system.py
+++ FuzzyLogicApp/fuzzy_system.py
@@ -57,6 +57,9 @@ class FuzzySystem():
         self.AntecedentDefuzzifiedMethodMembershipFunctionsByAntecedentName = dict();
         self.ConsequentDefuzzifiedMethodMembershipFunctionsByConsequentName = dict();
 
+        # antecedents don't have defuzzified values
+        self.ConsequentDefuzzifiedValuesByConsequentName = dict();
+
         self.ControlSystem = None;
         self.ControlSystemSimulation = None;
 
@@ -227,10 +230,18 @@ class FuzzySystem():
             self.ConsequentRangesByName[name] = consequentValueRange;
 
             # defaults to centroid
-            self.ConsequentsByName[name] = ctrl.Consequent(\
-                consequentValueRange,\
-                name,\
-                str(DefuzzifyingMethodEnum.CentroidOfArea));
+            self.ConsequentsByName[name] = ctrl.Consequent(consequentValueRange, name);
+            self.ConsequentDefuzzifiedValuesByConsequentName[name] = dict();
+
+            for method in DefuzzifyingMethodEnum.Values():
+                # a bit hacky, no need to create a duplicate consequent
+                if method == DefuzzifyingMethodEnum.CentroidOfArea:
+                    self.ConsequentDefuzzifiedValuesByConsequentName[name][method.Name] =\
+                        self.ConsequentsByName[name];
+                    continue;
+
+                self.ConsequentDefuzzifiedValuesByConsequentName[name][method.Name] =\
+                    ctrl.Consequent(consequentValueRange, name, str(method));
 
         return self.ConsequentsByName;
 
@@ -369,6 +380,15 @@ class FuzzySystem():
             consequent.view(sim = self.ControlSystemSimulation);
             consequentValue = self.ControlSystemSimulation.output[name];
             print("\t" + name + " = " + str(consequentValue));
+        return;
+
+    def PrintDefuzzifiedConsequentValues(self):
+        print("\nDefuzzified Consequent Values:");
+        for name, defuzzifiedConsequentsByMethod in self.ConsequentDefuzzifiedValuesByConsequentName.items():
+            print("\t" + name)
+            for methodName, consequent in defuzzifiedConsequentsByMethod.items():
+                consequentValue = self.ControlSystemSimulation.output[name];
+                print("\t\t" + methodName + " - " + str(consequentValue));
         return;
 
     def PlotAntecedents(self):
--- FuzzyLogicApp/main.py
+++ FuzzyLogicApp/main.py
@@ -14,6 +14,7 @@ def main():
     fuzzySystem.PlotConsequents();
     #fuzzySystem.PlotRules();
 
+    fuzzySystem.PrintDefuzzifiedConsequentValues();
     fuzzySystem.PlotDefuzzifiedCentroidConsequentValues();
 
     fuzzySystem.ShowPlots();
