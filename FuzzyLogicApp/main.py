938cba543c5701561e8c90ffacbdd196bd60b969
Georgi <45877509+GeorgeVelikov@users.noreply.github.com>
2020-12-07 13:48:20 +0000

    Revert "Revert "remove confused code""
    This reverts commit 522d0bb40cc78b44d05a210958800d75dbf91cb8.

--- FuzzyLogicApp/main.py
+++ FuzzyLogicApp/main.py
@@ -14,7 +14,6 @@ def main():
     fuzzySystem.PlotConsequents();
     #fuzzySystem.PlotRules();
 
-    fuzzySystem.PrintDefuzzifiedConsequentValues();
     fuzzySystem.PlotDefuzzifiedCentroidConsequentValues();
 
     fuzzySystem.ShowPlots();
