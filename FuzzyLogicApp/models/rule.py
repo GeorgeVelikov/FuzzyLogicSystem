4949864c49f47e88e79f16dd3875b822a89e0e00
Georgi <45877509+GeorgeVelikov@users.noreply.github.com>
2020-12-07 20:05:58 +0000

    tidy up rule.py
    finally, this is now somewhat readable

--- FuzzyLogicApp/models/rule.py
+++ FuzzyLogicApp/models/rule.py
@@ -28,18 +28,80 @@ class Rule:
             .split(":")[0]\
             .strip()
 
+        self.Name = ruleName
+
         # remove parsed data
         ruleText = ruleText\
-            .split(":")[1]\
+            .split("if ")[1]\
             .strip()
 
-        # first condition
+        firstCondition = self.GetCondition(ruleText);
+
+        self.Conditions\
+            .append(firstCondition);
+
+        # remove parsed data
+        ruleText = ruleText\
+            .split(firstCondition.VariableValue)[1][firstCondition.ClosingBracketsCount:]\
+            .strip()
+
+        # text to get result from
+        resultText = ruleText\
+            .split(" then ")[1]\
+            .strip()
+
+        resultCondition = self.GetResult(resultText);
+        self.Result = resultCondition;
+
+        # remove parsed data
+        ruleText = ruleText\
+            .split(" then ")[0]\
+            .strip()
+
+        # chained and/or connectives, this is just a hacky way of making sure
+        # we split all connectives.
+        # this however might break on unknown connectives.
+        chainedConditions = ruleText\
+            .replace("and ", "#and# ")\
+            .replace("or ", "#or# ")\
+            .split("#");
+
+        # TODO: get rid of empty entries, couldn't figure out a neater way to do it. Definitely hacky
+        if "" in chainedConditions:
+            chainedConditions.remove("");
+
+        # add subsequent conditions
+        for i in range(0, len(chainedConditions), 2):
+            connective = chainedConditions[i];
+            variable = chainedConditions[i+1];
+
+            chainedVariableConnectiveText = connective.strip();
+
+            chainedVariableConnectiveEnum = \
+                LogicalConnectiveEnum.And if chainedVariableConnectiveText == str(LogicalConnectiveEnum.And) \
+                else LogicalConnectiveEnum.Or if chainedVariableConnectiveText == str(LogicalConnectiveEnum.Or) \
+                else LogicalConnectiveEnum._None
+
+            if (chainedVariableConnectiveEnum == LogicalConnectiveEnum._None):
+                # something not right, cannot have subsequent conditions with
+                # None or unknown connectives
+                print("Incorrect Logical Connective used for subsequent condition - ", chainedVariableConnectiveText)
+                continue
+
+            chainedCondition = self.GetCondition(variable);
+            chainedCondition.LogicalConnective = chainedVariableConnectiveEnum;
+
+            self.Conditions.append(chainedCondition);
+        return;
+
+    def GetCondition(self, ruleText):
+        variableName = str();
+        variableValue = str();
         variableIsNegated = False;
         variableOpeningBrackets = 0;
         variableClosingBrackets = 0;
 
         variableName = ruleText\
-            .split("if")[1]\
             .split("is")[0]\
             .strip()
 
@@ -65,135 +127,53 @@ class Rule:
             variableClosingBrackets = variableValue.count(")");
             variableValue = variableValue.replace(")", str());
 
-        # remove parsed data
-        ruleText = ruleText\
-            .split(variableValue)[1][variableClosingBrackets:]\
-            .strip()
-
-        # set the result
-        positiveRuleResult = ruleText\
-            .split(" then ")[1]\
-            .strip()
-
-        resultVariableIsNegated = False;
-        resultOpeningBrackets = 0;
-        resultClosingBrackets = 0;
-
-        resultVariableName = positiveRuleResult\
-            .split(" is ")[0]\
-            .strip()
-
-        if "(" in resultVariableName:
-            resultOpeningBrackets = resultVariableName.count("(");
-            resultVariableName = resultVariableName.replace("(", str());
-
-        resultVariableValue = positiveRuleResult\
-            .split(" is ")[1]\
-            .strip()
-
-        if "not" in resultVariableValue:
-            resultVariableValue = ositiveRuleResult\
-                .split(" is ")[2]\
-                .strip()
-            resultVariableIsNegated = True;
-
-        if ")" in resultVariableValue:
-            resultClosingBrackets = resultVariableValue.count(")");
-            resultVariableValue = resultVariableValue.replace(")", str());
-
-        # removing parsed data
-        ruleText = ruleText\
-            .split(" then ")[0]\
-            .strip()
-
-        # chained and/or connectives, this is just a hacky way of making sure
-        # we split all connectives.
-        # this however might break on unknown connectives.
-        chainedConditions = ruleText\
-            .replace("and ", "#and# ")\
-            .replace("or ", "#or# ")\
-            .split("#");
-
-        # TODO: get rid of empty entries, couldn't figure out a neater way to do it. Definitely hacky
-        if "" in chainedConditions:
-            chainedConditions.remove("");
-
-        # actual Initialization
-        firstCondition = RuleCondition(\
+        return RuleCondition(\
             LogicalConnectiveEnum._None,\
             variableIsNegated,\
             variableOpeningBrackets,\
             variableClosingBrackets,\
             variableName,\
-            variableValue)
+            variableValue);
 
-        self.Result = RuleCondition(\
-           LogicalConnectiveEnum._None,\
-           resultVariableIsNegated,\
-           resultOpeningBrackets,\
-           resultClosingBrackets,\
-           resultVariableName,\
-           resultVariableValue)
+    def GetResult(self, resultText):
+        variableName = str();
+        variableValue = str();
+        variableIsNegated = False;
+        variableOpeningBrackets = 0;
+        variableClosingBrackets = 0;
 
-        self.Conditions\
-            .append(firstCondition)
+        variableName = resultText\
+            .split("is")[0]\
+            .strip()\
+            .split(" ")[0]\
+            .strip()
 
-        # add subsequent conditions
-        for i in range(0, len(chainedConditions), 2):
-            connective = chainedConditions[i];
-            variable = chainedConditions[i+1];
+        if "(" in variableName:
+            variableOpeningBrackets = variableName.count("(");
+            variableName = variableName.replace("(", str());
 
-            chainedVariableConnectiveText = connective.strip();
+        variableValue = resultText\
+            .split("is")[1]\
+            .strip()\
+            .split(" ")[0]\
+            .strip()
 
-            chainedVariableConnectiveEnum = \
-                LogicalConnectiveEnum.And if chainedVariableConnectiveText == str(LogicalConnectiveEnum.And) \
-                else LogicalConnectiveEnum.Or if chainedVariableConnectiveText == str(LogicalConnectiveEnum.Or) \
-                else LogicalConnectiveEnum._None
-
-            if (chainedVariableConnectiveEnum == LogicalConnectiveEnum._None):
-                # something not right, cannot have subsequent conditions with
-                # None or unknown connectives
-                print("Incorrect Logical Connective used for subsequent condition - ", chainedVariableConnectiveText)
-                continue
-
-            chainedVariableIsNegated = False;
-            chainedVariableOpeningBrackets = 0;
-            chainedVariableClosingBrackets = 0;
-
-            chainedVariableName = variable\
-                .split(" is ")[0]\
-                .split(" ")[1]\
-                .strip();
-
-            if "(" in chainedVariableName:
-                chainedVariableOpeningBrackets = chainedVariableName.count("(");
-                chainedVariableName = chainedVariableName.replace("(", str());
-
-            chainedVariableValue = variable\
-                .split(" is ")[1]\
+        if "not" in variableValue:
+            variableValue = variableValue\
+                .split("is")[1]\
                 .strip()\
-                .split(" ")[0]\
-                .strip();
+                .split(" ")[1]\
+                .strip()
+            variableIsNegated = True;
 
-            if "not" in chainedVariableValue:
-                chainedVariableValue = variable\
-                    .split(" is ")[1]\
-                    .strip()\
-                    .split(" ")[1]\
-                    .strip();
-                chainedVariableIsNegated = True;
+        if ")" in variableValue:
+            variableOpeningBrackets = variableValue.count(")");
+            variableValue = variableValue.replace(")", str());
 
-            if ")" in chainedVariableValue:
-                chainedVariableClosingBrackets = chainedVariableValue.count(")");
-                chainedVariableValue = chainedVariableValue.replace(")", str());
-
-            self.Conditions.append(RuleCondition(\
-                chainedVariableConnectiveEnum,\
-                chainedVariableIsNegated,\
-                chainedVariableOpeningBrackets,\
-                chainedVariableClosingBrackets,\
-                chainedVariableName,\
-                chainedVariableValue));
-
-        self.Name = ruleName
-        return;
+        return RuleCondition(\
+            LogicalConnectiveEnum._None,\
+            variableIsNegated,\
+            variableOpeningBrackets,\
+            variableClosingBrackets,\
+            variableName,\
+            variableValue);
